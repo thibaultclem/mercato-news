2b7e834e990ebdbdb58721354ea6774b471165d3
ThibaultClement <thibault.clem@gmail.com>
2016-05-27 16:33:18 +0200

    Display posts list

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-var app = angular.module('mercatoNews', [])
+//Include https://github.com/angular-ui/AngularJS-Atom snippet shortcuts
+
+var app = angular.module('mercatoNews', []) //ngma
 
 app.controller('MainCtrl', [
   '$scope',
-  function($scope) {
-    $scope.test = "Hello world !"
+  function($scope) { //ngdlf
+    $scope.test = "Hello world !"; //ngv
+    $scope.rumors = [
+      {title: 'Ibrahimovic in Arsenal', upvotes: 5},
+      {title: 'Neymar in Paris Saint Germain', upvotes: 2},
+      {title: 'Messi in FC Nantes', upvotes: 14},
+      {title: 'Pogba in Bayern', upvotes: 7},
+      {title: 'Kante in Manchester', upvotes: 5}
+    ];
   }
 ])
