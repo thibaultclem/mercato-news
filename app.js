c5e434d95ddd0cc165a30fb153a2afe40efcfe3b
ThibaultClement <thibault.clem@gmail.com>
2016-05-30 17:02:17 +0200

    Migrate data to service instead of controller

--- app.js
+++ app.js
@@ -4,15 +4,10 @@ var app = angular.module('mercatoNews', []) //ngma
 
 app.controller('MainCtrl', [
   '$scope',
-  function($scope) { //ngdlf
+  'rumors',
+  function($scope, rumors) { //ngdlf
     $scope.test = "Hello world !"; //ngv
-    $scope.rumors = [
-      {title: 'Ibrahimovic in Arsenal', link: 'http://metro.co.uk/2016/04/28/arsenal-to-offer-two-year-contract-to-seal-transfer-of-zlatan-ibrahimovic-5846727/', upvotes: 5},
-      {title: 'Neymar in Paris Saint Germain', link: 'http://www.mercatoparis.fr/neymar-psg', upvotes: 2},
-      {title: 'Messi in FC Nantes', upvotes: 14},
-      {title: 'Pogba in Bayern', upvotes: 7},
-      {title: 'Kante in Manchester', link: 'https://www.theguardian.com/football/2016/may/26/football-transfer-rumours-manchester-united-ngolo-kante', upvotes: 11}
-    ];
+    $scope.rumors = rumors.rumors;
     $scope.addRumor = function() { //ngf
       if (!$scope.title || $scope.title === '') { return; }//prevent blank title post by user
       $scope.rumors.push({
@@ -28,3 +23,16 @@ app.controller('MainCtrl', [
     };
   }
 ])
+
+app.factory('rumors', [function() {
+  var o = {
+    rumors: [
+      {title: 'Ibrahimovic in Arsenal', link: 'http://metro.co.uk/2016/04/28/arsenal-to-offer-two-year-contract-to-seal-transfer-of-zlatan-ibrahimovic-5846727/', upvotes: 5},
+      {title: 'Neymar in Paris Saint Germain', link: 'http://www.mercatoparis.fr/neymar-psg', upvotes: 2},
+      {title: 'Messi in FC Nantes', upvotes: 14},
+      {title: 'Pogba in Bayern', upvotes: 7},
+      {title: 'Kante in Manchester', link: 'https://www.theguardian.com/football/2016/may/26/football-transfer-rumours-manchester-united-ngolo-kante', upvotes: 11}
+    ]
+  };
+  return o;
+}])
