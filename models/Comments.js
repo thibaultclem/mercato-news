cf463fc4617a2c4b5b7dd8951f624a9491576b5b
ThibaultClement <thibault.clem@gmail.com>
2016-06-02 14:53:57 +0200

    Add comments routes

--- models/Comments.js
+++ models/Comments.js
@@ -7,4 +7,9 @@ var CommentSchema = new mongoose.Schema({
   rumor: { type: mongoose.Schema.Types.ObjectId, ref: 'Rumor' }
 });
 
+CommentSchema.methods.upvote = function(cb) {
+  this.upvotes += 1;
+  this.save(cb);
+};
+
 mongoose.model('Comment', CommentSchema);
--- routes/index.js
+++ routes/index.js
@@ -46,7 +46,11 @@ router.param('rumor', function(req, res, next, id) {
 
 /* GET rumor */
 router.get('/rumors/:rumor', function(req, res) {
-  res.json(req.rumor);
+  req.rumor.populate('comments', function(err, rumor) {
+    if (err) {return next(err); }
+
+    res.json(rumor);
+  });
 });
 
 
@@ -56,6 +60,45 @@ router.put('/rumors/:rumor/upvote', function(req, res, next) {
     if (err) { return next(err); }
 
     res.json(rumor);
+  });
+});
+
+/* POST comment */
+router.post('/rumors/:rumor/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.rumor = req.rumor;
+
+  comment.save(function(err, comment){
+    if(err){ return next(err); }
+
+    req.rumor.comments.push(comment);
+    req.rumor.save(function(err, post) {
+      if(err){ return next(err); }
+
+      res.json(comment);
+    });
+  });
+});
+
+/* comment PARAM */
+router.param('comment', function(req, res, next, id) {
+  var query = Comment.findById(id);
+
+  query.exec(function (err, comment){
+    if (err) { return next(err); }
+    if (!comment) { return next(new Error('can\'t find comment')); }
+
+    req.comment = comment;
+    return next();
+  });
+});
+
+/* PUT upvote comment */
+router.put('/rumors/:rumor/comments/:comment/upvote', function(req, res, next) {
+  req.comment.upvote(function(err, comment){
+    if (err) { return next(err); }
+
+    res.json(comment);
   });
 });
 
